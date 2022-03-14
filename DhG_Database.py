db946331be163662efb83460525fcb7548ec050c
David Haworth <dh@thelancashireman.org>
2022-03-14 20:55:25 +0100

    Add GPL header

--- DhG_Database.py
+++ DhG_Database.py
@@ -1,11 +1,30 @@
 #!/usr/bin/python3
 #
+# (c) David Haworth
+#
+# This file is part of DhG.
+#
+# DhG is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# DhG is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with DhG.  If not, see <http://www.gnu.org/licenses/>.
+
+
+
 # A class to represent the entire database
 #
 # The persons in the database are stored in an array indexed by the unique ID
 # The order in which the elements arrice is not known, so the gaps in the array are filled
 # with None as the loading proceeeds.
-
+#
 class Database:
 	def __init__(self):
 		self.persons = []
--- DhG_Person.py
+++ DhG_Person.py
@@ -1,6 +1,21 @@
 #!/usr/bin/python3
-
-# Person class - represent a person in the database
+#
+# (c) David Haworth
+#
+# This file is part of DhG.
+#
+# DhG is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# DhG is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with DhG.  If not, see <http://www.gnu.org/licenses/>.
 
 import os
 import sys
@@ -8,6 +23,9 @@ from DhG_Event import Event
 
 evchars = set(['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '?'])
 
+
+# Person class - represent a person in the database
+#
 class Person:
 	def __init__(self):
 		self.filename = None
